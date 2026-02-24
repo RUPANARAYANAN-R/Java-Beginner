0018ec0394fccb8c552ffea3b6c835f492c93b9c
RUPANARAYANAN R <rupanarayanan333@gmail.com>
2026-02-24 10:53:16 +0530

    2nd Program

---
+++ main.java
@@ -0,0 +1,13 @@
+class Program{
+    int x = 5;
+    public void x1(){
+        System.out.println("Name : " + x); 
+    }
+    
+}
+    
+public class main {
+    public static void main(String[] args) {
+    Program a=new Program();
+    a.x1();
+}}
