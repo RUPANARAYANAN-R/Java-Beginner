6ddde2a311189a5dae930d73ebce7db1bc5963f8
RUPANARAYANAN R <rupanarayanan333@gmail.com>
2026-02-24 10:29:18 +0530

    1st Program

--- oops1.java
+++ oops1.java
@@ -3,6 +3,8 @@ public class oops1 {
 
   public static void main(String[] args) {
     oops1 myObj = new oops1();
+    oops1 myObj2 = new oops1();
     System.out.println(myObj.x);
+    System.out.println(myObj2.x);
   }
 }
