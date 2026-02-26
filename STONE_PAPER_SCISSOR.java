23d9bd5f72f1f65100cded8785f224c22999d89d
RUPANARAYANAN R <rupanarayanan333@gmail.com>
2026-02-26 10:55:57 +0530

    Assessment

--- STONE_PAPER_SCISSOR.java
+++ STONE_PAPER_SCISSOR.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class spc {
+public class STONE_PAPER_SCISSOR {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String user= sc.next().toLowerCase();
